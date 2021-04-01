754dc788e104b6cefbae63e10c190cc094e2385f
Sparkles2000 <rucker.anna2000@gmail.com>
2021-04-01 03:55:39 -0500

    *addedTwoMoreCodesTo

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let num1 = 4;
 let num2 = 15.5;
 let multiply = num1 * num2;
 let random = Math.floor( Math.random() * 1000 ) + 1;
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max( 8, 12, 16, 20 );
